99f1836d053993f96612917345427b99f9215255
Julian Archila <julianarchilacaro@gmail.com>
2025-12-13 14:14:28 -0500

    db setup

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './src/core/db/migrations',
+  schema: './src/core/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
+
