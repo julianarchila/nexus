a7b905ebfab6ff60952268e7623a028d162d745d
Julian Archila <julianarchilacaro@gmail.com>
2025-12-14 06:14:50 -0500

    .

--- apps/web/src/app/api/inngest/route.ts
+++ apps/web/src/app/api/inngest/route.ts
@@ -1,7 +1,5 @@
 import { inngest } from "@/lib/inngest";
 import { serve } from "inngest/next";
-
-// Import new ingestion functions
 import {
   gongAdapter,
   gmailAdapter,
@@ -9,22 +7,7 @@ import {
   applyExtraction,
 } from "@/core/ingestion";
 
-// Import legacy functions (will be deprecated)
-import { functions as legacyFunctions } from "@/core/events";
-
-// Combine all functions
-const allFunctions = [
-  // New ingestion pipeline
-  gongAdapter,
-  gmailAdapter,
-  processEvent,
-  applyExtraction,
-  // Legacy functions (TODO: remove after migration)
-  ...legacyFunctions,
-];
-
-// Create an API that serves all functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: allFunctions,
+  functions: [gongAdapter, gmailAdapter, processEvent, applyExtraction],
 });
