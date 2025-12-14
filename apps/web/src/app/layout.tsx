3e6cd7fd6186ce9f2ac3fe371c2f9a7a824ea54e
Julian Archila <julianarchilacaro@gmail.com>
2025-12-14 13:32:24 -0500

    base setup

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -7,6 +7,10 @@ import "./globals.css";
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
 
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -37,6 +41,15 @@ export default function RootLayout({
           <AppSidebar />
           <main className="flex-1 w-full">
             <TRPCReactProvider>
+              <NextSSRPlugin
+                /**
+                 * The `extractRouterConfig` will extract **only** the route configs
+                 * from the router to prevent additional information from being
+                 * leaked to the client. The data passed to the client is the same
+                 * as if you were to fetch `/api/uploadthing` directly.
+                 */
+                routerConfig={extractRouterConfig(ourFileRouter)}
+              />
               {children}
               <TanStackDevtools
                 plugins={[
